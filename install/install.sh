fa1de10d158e2d4fe5e706a6a149a6a17a46dcc7
Diljit PR <diljitpr@gmail.com>
2017-02-04 00:10:34 +0530

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -9,6 +9,7 @@ then
     sudo LC_ALL=en_US.UTF-8 add-apt-repository ppa:ondrej/php;
     sudo apt-get update;
     sudo apt-get -y install apache2;
+    sudo apt-get install mysql-server
     sudo apt-get -y install php7.0 libapache2-mod-php7.0 php7.0-mcrypt phpmyadmin;
     sudo apt install -y php7.0-mbstring php7.0-zip php7.0-xml
     sudo apt-get install php7.0-mysql
